45a0ce91767bed78b7c3934d580af89c8800da76
Quang Minh Nguyen <minhnguyen181199@gmail.com>
2019-02-23 22:45:07 +0200

    Add navlink from react-router-dom and adjust UI

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => (
 	<div className="navigation-bar">
-		<span>About</span>
-		<span>Portfolio</span>
-		<span>Resume</span>
-		<span>Contact</span>
+		<NavLink to='/' exact activeStyle={
+			{color: 'blue'}
+		}>About</NavLink>
+		<NavLink to='/Portfolio' exact activeStyle={
+			{color: 'blue'}
+		}>Portfolio</NavLink>
+		<NavLink to='/Resume' exact activeStyle={
+			{color: 'blue'}
+		}>Resume</NavLink>
+		<NavLink to='/Contact' exact activeStyle={
+			{color: 'blue'}
+		}>Contact</NavLink>
 	</div>
 );
 
